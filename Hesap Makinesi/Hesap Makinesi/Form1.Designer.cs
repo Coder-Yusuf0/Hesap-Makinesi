8da52d187faf495fa5d175e0a6f8e5d467e82640
Coder-Yusuf0 <115824108+Coder-Yusuf0@users.noreply.github.com>
2022-10-17 21:40:43 +0300

    Add files via upload

---
+++ Hesap Makinesi/Hesap Makinesi/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace Hesap_Makinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.eşittir = new System.Windows.Forms.Button();
+            this.nokta = new System.Windows.Forms.Button();
+            this.çarpma = new System.Windows.Forms.Button();
+            this.bölme = new System.Windows.Forms.Button();
+            this.tuş0 = new System.Windows.Forms.Button();
+            this.toplama = new System.Windows.Forms.Button();
+            this.çıkarma = new System.Windows.Forms.Button();
+            this.tuş9 = new System.Windows.Forms.Button();
+            this.tuş8 = new System.Windows.Forms.Button();
+            this.tuş7 = new System.Windows.Forms.Button();
+            this.tuş6 = new System.Windows.Forms.Button();
+            this.tuş5 = new System.Windows.Forms.Button();
+            this.tuş4 = new System.Windows.Forms.Button();
+            this.tuş3 = new System.Windows.Forms.Button();
+            this.tuş2 = new System.Windows.Forms.Button();
+            this.tuş1 = new System.Windows.Forms.Button();
+            this.c = new System.Windows.Forms.Button();
+            this.silme = new System.Windows.Forms.Button();
+            this.ekranlabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ekranlabel);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(344, 58);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ekran";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.silme);
+            this.groupBox2.Controls.Add(this.c);
+            this.groupBox2.Controls.Add(this.eşittir);
+            this.groupBox2.Controls.Add(this.nokta);
+            this.groupBox2.Controls.Add(this.çarpma);
+            this.groupBox2.Controls.Add(this.bölme);
+            this.groupBox2.Controls.Add(this.tuş0);
+            this.groupBox2.Controls.Add(this.toplama);
+            this.groupBox2.Controls.Add(this.çıkarma);
+            this.groupBox2.Controls.Add(this.tuş9);
+            this.groupBox2.Controls.Add(this.tuş8);
+            this.groupBox2.Controls.Add(this.tuş7);
+            this.groupBox2.Controls.Add(this.tuş6);
+            this.groupBox2.Controls.Add(this.tuş5);
+            this.groupBox2.Controls.Add(this.tuş4);
+            this.groupBox2.Controls.Add(this.tuş3);
+            this.groupBox2.Controls.Add(this.tuş2);
+            this.groupBox2.Controls.Add(this.tuş1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(0, 64);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(344, 271);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tuş Takımı";
+            // 
+            // eşittir
+            // 
+            this.eşittir.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.eşittir.Location = new System.Drawing.Point(270, 180);
+            this.eşittir.Name = "eşittir";
+            this.eşittir.Size = new System.Drawing.Size(60, 85);
+            this.eşittir.TabIndex = 17;
+            this.eşittir.Text = "=";
+            this.eşittir.UseVisualStyleBackColor = true;
+            // 
+            // nokta
+            // 
+            this.nokta.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.nokta.Location = new System.Drawing.Point(138, 208);
+            this.nokta.Name = "nokta";
+            this.nokta.Size = new System.Drawing.Size(60, 57);
+            this.nokta.TabIndex = 16;
+            this.nokta.Text = ".";
+            this.nokta.UseVisualStyleBackColor = true;
+            // 
+            // çarpma
+            // 
+            this.çarpma.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.çarpma.Location = new System.Drawing.Point(204, 145);
+            this.çarpma.Name = "çarpma";
+            this.çarpma.Size = new System.Drawing.Size(60, 57);
+            this.çarpma.TabIndex = 15;
+            this.çarpma.Text = "*";
+            this.çarpma.UseVisualStyleBackColor = true;
+            // 
+            // bölme
+            // 
+            this.bölme.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bölme.Location = new System.Drawing.Point(204, 208);
+            this.bölme.Name = "bölme";
+            this.bölme.Size = new System.Drawing.Size(60, 57);
+            this.bölme.TabIndex = 14;
+            this.bölme.Text = "/";
+            this.bölme.UseVisualStyleBackColor = true;
+            // 
+            // tuş0
+            // 
+            this.tuş0.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.tuş0.Location = new System.Drawing.Point(6, 208);
+            this.tuş0.Name = "tuş0";
+            this.tuş0.Size = new System.Drawing.Size(126, 57);
+            this.tuş0.TabIndex = 13;
+            this.tuş0.Text = "0";
+            this.tuş0.UseVisualStyleBackColor = true;
+            this.tuş0.Click += new System.EventHandler(this.tuş0_Click);
+            // 
+            // toplama
+            // 
+            this.toplama.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.toplama.Location = new System.Drawing.Point(204, 82);
+            this.toplama.Name = "toplama";
+            this.toplama.Size = new System.Drawing.Size(60, 57);
+            this.toplama.TabIndex = 12;
+            this.toplama.Text = "+";
+            this.toplama.UseVisualStyleBackColor = true;
+            this.toplama.Click += new System.EventHandler(this.toplama_Click);
+            // 
+            // çıkarma
+            // 
+            this.çıkarma.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.çıkarma.Location = new System.Drawing.Point(204, 19);
+            this.çıkarma.Name = "çıkarma";
+            this.çıkarma.Size = new System.Drawing.Size(60, 57);
+            this.çıkarma.TabIndex = 11;
+            this.çıkarma.Text = "-";
+            this.çıkarma.UseVisualStyleBackColor = true;
+            // 
+            // tuş9
+            // 
+            this.tuş9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.tuş9.Location = new System.Drawing.Point(138, 145);
+            this.tuş9.Name = "tuş9";
+            this.tuş9.Size = new System.Drawing.Size(60, 57);
+            this.tuş9.TabIndex = 10;
+            this.tuş9.Text = "9";
+            this.tuş9.UseVisualStyleBackColor = true;
+            this.tuş9.Click += new System.EventHandler(this.tuş9_Click);
+            // 
+            // tuş8
+            // 
+            this.tuş8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.tuş8.Location = new System.Drawing.Point(72, 145);
+            this.tuş8.Name = "tuş8";
+            this.tuş8.Size = new System.Drawing.Size(60, 57);
+            this.tuş8.TabIndex = 9;
+            this.tuş8.Text = "8";
+            this.tuş8.UseVisualStyleBackColor = true;
+            this.tuş8.Click += new System.EventHandler(this.tuş8_Click);
+            // 
+            // tuş7
+            // 
+            this.tuş7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.tuş7.Location = new System.Drawing.Point(6, 145);
+            this.tuş7.Name = "tuş7";
+            this.tuş7.Size = new System.Drawing.Size(60, 57);
+            this.tuş7.TabIndex = 8;
+            this.tuş7.Text = "7";
+            this.tuş7.UseVisualStyleBackColor = true;
+            this.tuş7.Click += new System.EventHandler(this.tuş7_Click);
+            // 
+            // tuş6
+            // 
+            this.tuş6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.tuş6.Location = new System.Drawing.Point(138, 82);
+            this.tuş6.Name = "tuş6";
+            this.tuş6.Size = new System.Drawing.Size(60, 57);
+            this.tuş6.TabIndex = 7;
+            this.tuş6.Text = "6";
+            this.tuş6.UseVisualStyleBackColor = true;
+            this.tuş6.Click += new System.EventHandler(this.tuş6_Click);
+            // 
+            // tuş5
+            // 
+            this.tuş5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.tuş5.Location = new System.Drawing.Point(72, 82);
+            this.tuş5.Name = "tuş5";
+            this.tuş5.Size = new System.Drawing.Size(60, 57);
+            this.tuş5.TabIndex = 6;
+            this.tuş5.Text = "5";
+            this.tuş5.UseVisualStyleBackColor = true;
+            this.tuş5.Click += new System.EventHandler(this.tuş5_Click);
+            // 
+            // tuş4
+            // 
+            this.tuş4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.tuş4.Location = new System.Drawing.Point(6, 82);
+            this.tuş4.Name = "tuş4";
+            this.tuş4.Size = new System.Drawing.Size(60, 57);
+            this.tuş4.TabIndex = 5;
+            this.tuş4.Text = "4";
+            this.tuş4.UseVisualStyleBackColor = true;
+            this.tuş4.Click += new System.EventHandler(this.tuş4_Click);
+            // 
+            // tuş3
+            // 
+            this.tuş3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.tuş3.Location = new System.Drawing.Point(138, 19);
+            this.tuş3.Name = "tuş3";
+            this.tuş3.Size = new System.Drawing.Size(60, 57);
+            this.tuş3.TabIndex = 4;
+            this.tuş3.Text = "3";
+            this.tuş3.UseVisualStyleBackColor = true;
+            this.tuş3.Click += new System.EventHandler(this.tuş3_Click);
+            // 
+            // tuş2
+            // 
+            this.tuş2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.tuş2.Location = new System.Drawing.Point(72, 19);
+            this.tuş2.Name = "tuş2";
+            this.tuş2.Size = new System.Drawing.Size(60, 57);
+            this.tuş2.TabIndex = 3;
+            this.tuş2.Text = "2";
+            this.tuş2.UseVisualStyleBackColor = true;
+            this.tuş2.Click += new System.EventHandler(this.tuş2_Click);
+            // 
+            // tuş1
+            // 
+            this.tuş1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.tuş1.Location = new System.Drawing.Point(6, 19);
+            this.tuş1.Name = "tuş1";
+            this.tuş1.Size = new System.Drawing.Size(60, 57);
+            this.tuş1.TabIndex = 2;
+            this.tuş1.Text = "1";
+            this.tuş1.UseVisualStyleBackColor = true;
+            this.tuş1.Click += new System.EventHandler(this.tuş1_Click);
+            // 
+            // c
+            // 
+            this.c.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.c.Location = new System.Drawing.Point(270, 99);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(60, 75);
+            this.c.TabIndex = 18;
+            this.c.Text = "C";
+            this.c.UseVisualStyleBackColor = true;
+            this.c.Click += new System.EventHandler(this.c_Click);
+            // 
+            // silme
+            // 
+            this.silme.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.silme.Location = new System.Drawing.Point(270, 19);
+            this.silme.Name = "silme";
+            this.silme.Size = new System.Drawing.Size(60, 74);
+            this.silme.TabIndex = 19;
+            this.silme.Text = "←";
+            this.silme.UseVisualStyleBackColor = true;
+            // 
+            // ekranlabel
+            // 
+            this.ekranlabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ekranlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ekranlabel.Location = new System.Drawing.Point(12, 16);
+            this.ekranlabel.Name = "ekranlabel";
+            this.ekranlabel.Size = new System.Drawing.Size(326, 39);
+            this.ekranlabel.TabIndex = 0;
+            this.ekranlabel.Text = "0";
+            this.ekranlabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.ekranlabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 335);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Hesap Makinesi";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button eşittir;
+        private System.Windows.Forms.Button nokta;
+        private System.Windows.Forms.Button çarpma;
+        private System.Windows.Forms.Button bölme;
+        private System.Windows.Forms.Button tuş0;
+        private System.Windows.Forms.Button toplama;
+        private System.Windows.Forms.Button çıkarma;
+        private System.Windows.Forms.Button tuş9;
+        private System.Windows.Forms.Button tuş8;
+        private System.Windows.Forms.Button tuş7;
+        private System.Windows.Forms.Button tuş6;
+        private System.Windows.Forms.Button tuş5;
+        private System.Windows.Forms.Button tuş4;
+        private System.Windows.Forms.Button tuş3;
+        private System.Windows.Forms.Button tuş2;
+        private System.Windows.Forms.Button tuş1;
+        private System.Windows.Forms.Button silme;
+        private System.Windows.Forms.Button c;
+        private System.Windows.Forms.Label ekranlabel;
+    }
+}
+
---
+++ Hesap Makinesi/Hesap Makinesi/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hesap_Makinesi
+{
+    public partial class Form1 : Form
+    {
+        private char _işlem;// 
+        private bool _ekrantemizlenecek;//Değişkenler tanımlanıyor.
+        private int _ilksayı;//
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tuş1_Click(object sender, EventArgs e)
+        {
+            if (ekranlabel.Text == "0") ekranlabel.Text = "";//ekran da 0 yazıyorsa 0 'ı sil
+            {
+                ekranlabel.Text += "1";//buttona her tıklandığında ekrana rakamı ekler
+            }
+
+        }
+
+        private void tuş2_Click(object sender, EventArgs e)
+        {
+            if (ekranlabel.Text == "0") ekranlabel.Text = "";//ekran da 0 yazıyorsa 0 'ı sil
+            {
+                ekranlabel.Text += "2";//buttona her tıklandığında ekrana rakamı ekler
+            }
+        }
+
+        private void tuş3_Click(object sender, EventArgs e)
+        {
+            if (ekranlabel.Text == "0") ekranlabel.Text = "";//ekran da 0 yazıyorsa 0 'ı sil
+            {
+                ekranlabel.Text += "3";//buttona her tıklandığında ekrana rakamı ekler
+            }
+        }
+
+        private void tuş4_Click(object sender, EventArgs e)
+        {
+            if (ekranlabel.Text == "0") ekranlabel.Text = "";//ekran da 0 yazıyorsa 0 'ı sil
+            {
+                ekranlabel.Text += "4";//buttona her tıklandığında ekrana rakamı ekler
+            }
+        }
+
+        private void tuş5_Click(object sender, EventArgs e)
+        {
+            if (ekranlabel.Text == "0") ekranlabel.Text = "";//ekran da 0 yazıyorsa 0 'ı sil
+            {
+                ekranlabel.Text += "5";//buttona her tıklandığında ekrana rakamı ekler
+            }
+        }
+
+        private void tuş6_Click(object sender, EventArgs e)
+        {
+            if (ekranlabel.Text == "0") ekranlabel.Text = "";//ekran da 0 yazıyorsa 0 'ı sil
+            {
+                ekranlabel.Text += "6";//buttona her tıklandığında ekrana rakamı ekler
+            }
+        }
+
+        private void tuş7_Click(object sender, EventArgs e)
+        {
+            if (ekranlabel.Text == "0") ekranlabel.Text = "";//ekran da 0 yazıyorsa 0 'ı sil
+            {
+                ekranlabel.Text += "7";
+            }
+        }
+
+        private void tuş8_Click(object sender, EventArgs e)
+        {
+            if (ekranlabel.Text == "0") ekranlabel.Text = "";//ekran da 0 yazıyorsa 0 'ı sil
+            {
+                ekranlabel.Text += "8";//buttona her tıklandığında ekrana rakamı ekler
+            }
+        }
+
+        private void tuş9_Click(object sender, EventArgs e)
+        {
+            if (ekranlabel.Text == "0") ekranlabel.Text = "";//ekran da 0 yazıyorsa 0 'ı sil
+            {
+                ekranlabel.Text += "9";//buttona her tıklandığında ekrana rakamı ekler
+            }
+        }
+
+        private void tuş0_Click(object sender, EventArgs e)
+        {
+            if (ekranlabel.Text == "0") ekranlabel.Text = "";//ekran da 0 yazıyorsa 0 'ı sil
+            {
+                ekranlabel.Text += "0";//buttona her tıklandığında ekrana rakamı ekler
+            }
+        }
+
+        private void c_Click(object sender, EventArgs e)
+        {
+            ekranlabel.Text = "";///Button a tıklandığında ekranı temizler
+        }
+
+        private void toplama_Click(object sender, EventArgs e)
+        {
+            _işlem = "+";//+ ya basılınca toplar
+            _ekrantemizlenecek = true;//sayıları ekrandan kaldırır ve işlemi yapıcağı diğer sayılara geçer
+            _ilksayı = Convert.ToInt32(ekranlabel.Text);//ekrandaki sayıları  kaydeder.
+        }
+    }
+}
